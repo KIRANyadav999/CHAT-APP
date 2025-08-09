0f23e3cf43feaf90e02ed1f2f64add3a2375c095
Ambati Kiran Babu <97722209+KIRANyadav999@users.noreply.github.com>
2025-08-09 17:01:25 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client"
