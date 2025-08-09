2ef07548ff967c14336ae704b1b6148804a18feb
Ambati Kiran Babu <97722209+KIRANyadav999@users.noreply.github.com>
2025-08-09 16:58:41 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom"
 import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
